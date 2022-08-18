42bc2d99c09832f63d2a6290e0469fd45c9cb661
Daniel Li <Dli53@outlook.com>
2022-08-18 16:10:08 -0400

    Associations Exercise

--- practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
@@ -1,2 +1,25 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
+
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :student_id,
+        class_name: :Enrollment
+
+    has_many :enrolled_courses,
+        through: :enrollments, # goes through own enrollments, not the table
+        source: :course # takes from the student through our own enrollments
+
+    has_one :course,
+        primary_key: :id,
+        foreign_key: :instructor_id,
+        class_name: :Course
 end
